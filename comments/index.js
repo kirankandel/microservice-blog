79333959f429ee688acd9fe52d3fabf92e9500b2
Kiran Kandel <theknownofficial@gmail.com>
2021-01-13 21:51:52 +0545

    Comment Server init

--- comments/index.js
+++ comments/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const CommentByPostsId = {};
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/posts/:id/comments", (req, res) => {});
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(CommentByPostsId[req.params.id] || []);
+});
 
-app.post("/posts/:id/comments", (req, res) => {});
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const content = req.body;
+  const comments = CommentByPostsId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  CommentByPostsId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
   console.log("Listening on 4001");
