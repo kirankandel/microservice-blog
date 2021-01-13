ad51385880266dbd24c55ee13b76f668edc60ea4
Kiran Kandel <theknownofficial@gmail.com>
2021-01-13 21:08:57 +0545

    Addition of gitignore and basic setup

--- posts/index.js
+++ posts/index.js
@@ -1,27 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
+
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = {
     id,
-    title,
+    title
   };
 
   res.status(201).send(posts[id]);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000...");
+app.listen(4000, () => {
+  console.log('Listening on 4000');
 });
